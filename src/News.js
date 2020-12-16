41b1b5a025de08a63de33b9d64847b84ff2519e7
Hamza Muhammad <hmuhammad@inths.org>
2020-12-16 00:16:23 -0500

    first commit newsapp

--- src/News.js
+++ src/News.js
@@ -7,7 +7,7 @@ const News = () => {
   console.log(newsData);
   const newsApi = async () => {
     const res = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=us&apiKey=9d33da08e78a4541a26669e980396fc0`
+      `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us&apiKey=9d33da08e78a4541a26669e980396fc0`
     );
     // console.log(res.data.articles);
     setNewsData(res.data.articles);
